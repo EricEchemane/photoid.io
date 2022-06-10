07770659bc568b79b731b2e69d42f2df506573c7
Eric Echemane <eechemane29@gmail.com>
2022-06-10 08:33:58 +0800

    setup ready for styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Button from 'styled/button';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Button> Hello </Button>
+    </div>
   );
 }
---
+++ styled/button.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    padding: 1rem 1.2rem;
+    width: max-content;
+    font-weight: 600;
+    background: hsla(0, 0%, 0%, 1);
+    color: white;
+    border-radius: 20px;
+`;
+
+export default Button;
