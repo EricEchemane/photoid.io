df25ea977c375bf910bd4d2af8f6977978b32d52
Eric Echemane <eechemane29@gmail.com>
2022-06-10 08:59:32 +0800

    styled-components removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Button from 'styled/button';
 
 export default function Home() {
   return (
     <div>
-      <Button> Hello </Button>
+      hello
     </div>
   );
 }
