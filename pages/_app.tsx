1e93eb0474ae0d409b414f329e75573f44c44216
Eric Echemane <eechemane29@gmail.com>
2022-06-10 09:48:44 +0800

    theming configured

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+// _app.tsx file
+import { GetServerSidePropsContext } from 'next';
+import { useState } from 'react';
+import { AppProps } from 'next/app';
+// install cookies-next package to manage cookies
+import { getCookie, setCookies } from 'cookies-next';
+import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App(props: AppProps & { colorScheme: ColorScheme; }) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+    // when color scheme is updated save it to cookie
+    setCookies('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
+  };
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{
+        colorScheme,
+        fontFamily: 'Inter, sans-serif'
+      }} withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
 
-export default MyApp;
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext; }) => ({
+  // get color scheme from cookie
+  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button, useMantineColorScheme } from '@mantine/core';
 import React from 'react';
 
 export default function Home() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   return (
-    <div>
-      hello
+    <div style={{ margin: '5rem' }}>
+      <Button onClick={() => toggleColorScheme()}> {colorScheme} </Button>
     </div>
   );
 }
